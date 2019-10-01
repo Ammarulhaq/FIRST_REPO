ba7693524097b06027462eba777a651097c54965
Ammar <ammar@gmail.com>
2019-10-01 14:45:25 +0500

    New file added

---
+++ mod.js
@@ -0,0 +1,20 @@
+
+
+var temp=
+{
+
+
+   //assigining properties to functions
+
+   Add:function Add_Numbers()
+   {
+       return 1;
+   },
+
+   Sub:function Subtract_Numbers()
+   {
+
+   }
+}
+module.exports.temp=temp;
+//console.log(module);
